2303b8cf090bb72f79f1ddb9d796ac07d8b96b32
r@pc01 <comiarln@gmail.com>
2022-09-06 13:57:34 +0800

    hi hello

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const blocks = document.querySelectorAll('p');
-const audio = document.querySelectorAll('audio');
+const audioNum = document.querySelectorAll('.audio--numbers');
+
+const hello = document.getElementById('hello');
+const audioHello = document.getElementById('audio--hello');
+
+const good = document.getElementById('good');
+const audioGood = document.getElementById('audio--good');
+
+hello.addEventListener('click', () => {
+  audioHello.play();
+});
+
+good.addEventListener('click', () => {
+  audioGood.play();
+});
 
 for (let i = 0; i < blocks.length; i++) {
   blocks[i].addEventListener('click', () => {
-    audio[i].play();
+    audioNum[i].play();
   });
 }
