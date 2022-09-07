866c383f516d543e609380eb562e678610d0f499
r@pc01 <comiarln@gmail.com>
2022-09-07 16:03:07 +0800

    shorten fx

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const blocks = document.querySelectorAll('p');
-const audioNum = document.querySelectorAll('.audio--numbers');
-
 const hello = document.getElementById('hello');
-const audioHello = document.getElementById('audio--hello');
-
 const good = document.getElementById('good');
+
+const audioNum = document.querySelectorAll('.audio--numbers');
+const audioHello = document.getElementById('audio--hello');
 const audioGood = document.getElementById('audio--good');
 
-hello.addEventListener('click', () => {
-  audioHello.play();
-});
-
-good.addEventListener('click', () => {
-  audioGood.play();
-});
+hello.addEventListener('click', () => audioHello.play());
+good.addEventListener('click', () => audioGood.play());
 
 for (let i = 0; i < blocks.length; i++) {
-  blocks[i].addEventListener('click', () => {
-    audioNum[i].play();
-  });
+  blocks[i].addEventListener('click', () => audioNum[i].play());
 }
